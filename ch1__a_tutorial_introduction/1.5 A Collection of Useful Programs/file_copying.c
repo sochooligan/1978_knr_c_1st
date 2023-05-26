cd18dcca71ca2305f6d78110f60b221ead25d9ad
sochooligan <sochooligan@163.com>
2023-05-26 10:45:25 +0800

    use functions getchar and putchar

--- ch1__a_tutorial_introduction/1.5 A Collection of Useful Programs/file_copying.c
+++ ch1__a_tutorial_introduction/1.5 A Collection of Useful Programs/file_copying.c
@@ -2,6 +2,10 @@
 
 main()    /* copy input to output; 1st version */
 {
+     /* declare c to b an int, so it can hold the value
+          which getchar returns.  It must be capable of
+          representing EOF in addition to all possible
+          char's */
      int c;
      
      c = getchar();
---
+++ ch1__a_tutorial_introduction/1.5 A Collection of Useful Programs/print_eof.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+main()    /* print end of file value */
+{
+     printf("EOF: %d\n", EOF);
+}
