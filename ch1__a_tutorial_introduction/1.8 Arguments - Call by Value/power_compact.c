1958a4aaddd714c4b57a26f6ab758aa908115edb
sochooligan <sochooligan@163.com>
2023-05-27 19:51:04 +0800

    call by value

---
+++ ch1__a_tutorial_introduction/1.8 Arguments - Call by Value/power_compact.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+main()    /* test power function */
+{
+     int i;
+
+     for (i = 0; i < 10; ++i)
+	  printf("%d %d %d\n", i, power(2, i), power(-3, i));
+}
+
+power(x, n)    /* raise x to n-th power; n > 0 */
+int x, n;
+{
+     int p;
+     
+     p = 1;
+     for (p = 1; n > 0; --n) {
+	  p = p * x;
+     }
+     
+     return p;
+}
